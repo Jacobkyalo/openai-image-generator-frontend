f78113ddaaa1e8360c186a1ec1a7a71bd46d3098
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-03-31 15:57:35 +0300

    updated form width

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,29 +10,31 @@ function Form() {
   };
   return (
     <section className="my-10">
-      <form
-        className="w-full max-w-[250px] mx-auto flex flex-col sm:flex-row justify-center items-center gap-6"
-        onSubmit={handleFormSubmit}
-      >
-        <input
-          type="text"
-          name="prompt"
-          className="border-2 border-gray-50 px-6 py-2 text-white text-[20px] rounded-[40px] bg-gray-800 active:bg-gray-800 focus:border-gray-50"
-          placeholder="Enter image to generate"
-          autoComplete="off"
-          minLength={4}
-          required
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-        />
-
-        <button
-          type="submit"
-          className="bg-gradient-to-r from-green-600 to-blue-500 hover:from-blue-500 hover:to-green-600 transition-all duration-500 text-white text-[24px] px-6 py-2 border-0 rounded-[40px]"
+      <div className="w-[300px] mx-auto bg-green-700">
+        <form
+          className="w-full flex flex-col sm:flex-row justify-center items-center gap-6"
+          onSubmit={handleFormSubmit}
         >
-          Generate
-        </button>
-      </form>
+          <input
+            type="text"
+            name="prompt"
+            className="border-2 border-blue-600 px-6 py-2 text-white text-[20px] rounded-[40px] bg-gray-800 outline-none"
+            placeholder="Enter image to generate"
+            autoComplete="off"
+            minLength={4}
+            required
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-green-600 to-blue-500 hover:from-blue-500 hover:to-green-600 transition-all duration-500 text-white text-[24px] px-6 py-2 border-0 rounded-[40px]"
+          >
+            Generate
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
