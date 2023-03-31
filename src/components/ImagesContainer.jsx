39568c9a145710848614b4acc02b1b1a3b98c010
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-03-31 17:04:37 +0300

    updated loading conditional

--- src/components/ImagesContainer.jsx
+++ src/components/ImagesContainer.jsx
@@ -6,42 +6,44 @@ function ImagesContainer() {
   const { images, loading } = useContext(ImageContext);
   return (
     <>
-      {images?.length > 1 ? (
-        <section className="my-8">
-          {loading ? (
-            <div className="flex justify-center text-center mt-4 ">
-              <span className="block">
-                <LineWave
-                  height="100"
-                  width="100"
-                  color="#2563eb"
-                  ariaLabel="line-wave"
-                  visible={true}
-                />
-              </span>
-            </div>
-          ) : (
-            <div
-              className={`${
-                images?.length > 1 ? "bg-gray-800" : "bg-gray-900"
-              } rounded-2xl`}
-            >
-              <div className="grid justify-center gap-3 items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-4 rounded-lg p-8">
-                {images?.map((image, index) => (
-                  <div className="w-full overflow-hidden" key={index}>
-                    <img
-                      src={image.url}
-                      alt="searched image"
-                      className="w-full sm:w-52 h-52 rounded-2xl object-cover"
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
-        </section>
+      {loading ? (
+        <div className="flex justify-center text-center mt-4 ">
+          <span className="block">
+            <LineWave
+              height="100"
+              width="100"
+              color="#2563eb"
+              ariaLabel="line-wave"
+              visible={true}
+            />
+          </span>
+        </div>
       ) : (
-        ""
+        <>
+          {images?.length > 1 ? (
+            <section className="my-8">
+              <div
+                className={`${
+                  images?.length > 1 ? "bg-gray-800" : "bg-gray-900"
+                } rounded-2xl`}
+              >
+                <div className="grid justify-center gap-3 items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-4 rounded-lg p-8">
+                  {images?.map((image, index) => (
+                    <div className="w-full overflow-hidden" key={index}>
+                      <img
+                        src={image.url}
+                        alt="searched image"
+                        className="w-full sm:w-52 h-52 rounded-2xl object-cover"
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </section>
+          ) : (
+            ""
+          )}
+        </>
       )}
     </>
   );
