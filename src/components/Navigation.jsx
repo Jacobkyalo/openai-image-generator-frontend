7e1384744bec7625963ab5f9273cc19f235a7942
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-04-05 17:49:19 +0300

    updated logo size in small devices

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 function Navigation() {
   return (
     <header className="py-5 flex items-center sticky w-full top-0 z-10 bg-gray-900">
-      <h2 className="bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-600 text-[30px] font-[600]">
+      <h2 className="bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-600 text-[18px] sm:text-[30px] font-[600]">
         🔥 Image Generator
       </h2>
     </header>
